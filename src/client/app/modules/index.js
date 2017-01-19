1a250c8d64e48b62dbc6913931ff2c4bf8ab1ea9
kopmba <slhenny30@gmail.com>
2017-01-19 02:41:05 +0100

    Update index.js

--- src/client/app/modules/index.js
+++ src/client/app/modules/index.js
@@ -4,11 +4,6 @@ import './layout';
 import './home';
 import './settings';
 import './profile';
-import './article';
-import './announcement';
-import './contact';
-import './album';
-import './artist';
 import './book';
 
 let appModules = angular.module('app.modules', [
@@ -17,11 +12,6 @@ let appModules = angular.module('app.modules', [
     'app.home',
     'app.settings',
     'app.profile',
-    'app.article',
-    'app.announcement',
-    'app.contact',
-    'app.album',
-    'app.artist',
     'app.book'
 ]);
 
